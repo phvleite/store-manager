1a10297bd715aaf7e82eed09a062aed3bf35c047
phvleite <phvleite@gmail.com>
2022-06-30 03:18:46 -0300

    corrigido falha que provocou queda do [REQ 01]

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,7 +4,7 @@ const productController = {
 
   async getById(req, res) {
     const { id } = await productService.validateParamsId(req.params);
-    await productService.checkIfExist(id);
+    await productService.checkIfExists(id);
     const product = await productService.getById(id);
     res.status(200).json(product);
   },
